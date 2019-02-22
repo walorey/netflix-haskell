ac39eb42954ffda5bf321853588a86d40ca643b1
walorey <facu-a77aque@hotmail.com.ar>
2019-02-22 19:25:14 -0300

    primer comit
    enunciado y puntos hechos hasta el 7

---
+++ netflix.hs
@@ -0,0 +1,111 @@
+data Serie = UnaSerie { 
+    nombre :: String, 
+    genero :: String,
+    duracion :: Int,
+    cantTemporadas :: Int,
+    calificaciones :: [Int],
+    esOriginalDeNetflis :: Bool
+} deriving (Eq, Show)
+
+tioGolpetazo = UnaSerie {
+    nombre = "One punch man",
+    genero = "Monito chino",
+    duracion = 24,
+    cantTemporadas = 1,
+    calificaciones = [5,3,4],
+    esOriginalDeNetflis = False
+}
+
+cosasExtranias = UnaSerie {
+    nombre = "Strangers things",
+    genero = "Misterio",
+    duracion = 50,
+    cantTemporadas = 2,
+    calificaciones = [3,3],
+    esOriginalDeNetflis = True
+}
+
+dbs = UnaSerie {
+    nombre = "Dragon ball supah",
+    genero = "Monito chino",
+    duracion = 150,
+    cantTemporadas = 5,
+    calificaciones = [],
+    esOriginalDeNetflis = False
+}
+
+espejoNegro = UnaSerie {
+    nombre = "Black mirrow",
+    genero = "Suspenso",
+    duracion = 123,
+    cantTemporadas = 4,
+    calificaciones = [2],
+    esOriginalDeNetflis = True
+}
+
+rompiendoMalo = UnaSerie {
+    nombre = "Breaking Bad",
+    genero = "Drama",
+    duracion = 200,
+    cantTemporadas = 5,
+    calificaciones = [],
+    esOriginalDeNetflis = False
+}
+
+treceRazonesPorque = UnaSerie {
+    nombre = "13 reasons why",
+    genero = "Drama",
+    duracion = 50,
+    cantTemporadas = 1,
+    calificaciones = [3,3,3],
+    esOriginalDeNetflis = True
+}
+
+-- PARTE 1
+
+-- 1 Crear maraton
+
+maratonAnime = [tioGolpetazo, dbs]
+
+-- 2 Saber la cantidad de series del maraton
+
+cuantasSeriesMaraton maraton = length maraton
+
+-- 3 Saber si una serie es popular
+
+esPopular serie = length (calificaciones serie) == 3 || length (calificaciones serie) > 3
+
+-- 4 Saber si la serie vale la pena
+
+serieValeLaPena serie = (cantTemporadas serie) > 1 && esPopular serie
+
+-- 5 Saber si un maraton vale la pena
+
+maratonValeLaPena maraton = (serieValeLaPena (head maraton) && serieValeLaPena (last maraton)) || elem (rompiendoMalo) maraton
+
+-- 6 Saber si un maraton repunta al final
+
+maratonRepunta maraton = not (maratonValeLaPena (primerMitad maraton) ) && maratonValeLaPena (segundaMitad maraton)
+
+primerMitad maraton = take (length maraton `div` 2) maraton
+
+segundaMitad maraton = drop (length maraton `div` 2) maraton
+
+--7 calcular calificaciones
+
+calificar serie = (sum (calificaciones serie)) `div` (length (calificaciones serie))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
